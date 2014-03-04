4ab9aba5517150f9ed2f3c1df5fab9ce57fe8a24
sqrtxx <sqrtxx@gmail.com>
2014-03-04 18:56:24 +0900

    Add color of powerline powerd by shibayu36

--- init.el
+++ init.el
@@ -62,7 +62,22 @@
 (setq jedi:complete-on-dot t)
 ;;; powerline
 (require 'powerline)
+(set-face-attribute 'mode-line nil
+		    :foreground "#fff"
+		    :background "#FF0066"
+		    :box nil)
+
+(set-face-attribute 'powerline-active1 nil
+		    :foreground "#fff"
+		    :background "#FF6699"
+		    :inherit 'mode-line)
+
+(set-face-attribute 'powerline-active2 nil
+		    :foreground "#000"
+		    :background "#ffaeb9"
+		    :inherit 'mode-line)
 (powerline-default-theme)
+
 ;;; linum: 行表示
 (require 'linum)
 (global-linum-mode t)
